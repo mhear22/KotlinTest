e4346e73506bffd689de7e1d869f5744b3c3429c
Mika Heares <mhear22@gmail.com>
2021-06-11 00:10:17 +1000

    inital commit

---
+++ src/test/java/kotlintest/AppTest.java
@@ -0,0 +1,20 @@
+package kotlintest;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
